535f8b4912b481c166cdbb441063fd9ca04315cc
Anastasiia Krasevych <nkrasevich@gmail.com>
2024-02-11 17:35:18 +0200

    task 3

--- homework_5.py
+++ homework_5.py
@@ -42,6 +42,75 @@ text = "Загальний дохід працівника складаєтьс�
 total_income = sum_profit(text, generator_numbers)
 print(f"Загальний дохід: {total_income}")
 
+# task 3
+import sys
+from collections import defaultdict
+
+def parse_log_line(line: str) -> dict:
+    '''Parse a log line and return a dictionary with parsed components.'''
+    components = line.strip().split()
+    if len(components) < 6:
+        print(f'Invalid log line: {line}')
+        return None  # Skip invalid log lines
+    return {
+        'datetime': components[0] + ' ' + components[1],
+        'level': components[2],
+        'message': ' '.join(components[5:])  # Concatenate remaining components as message
+    }
+
+
+def load_logs(file_path: str) -> list:
+    '''Load logs from a file.'''
+    logs = []
+    try:
+        with open(file_path, 'r') as fh:
+            for line in fh:
+                logs.append(parse_log_line(line))
+    except FileNotFoundError:
+        print('File not found.')
+    return logs
+
+def filter_logs_by_level(logs: list, level: str) -> list:
+    '''Filter logs by level.'''
+    return [log for log in logs if log['level'] == level]
+
+def count_logs_by_level(logs: list) -> dict:
+    '''Count logs by level.'''
+    counts = defaultdict(int)
+    for log in logs:
+        counts[log['level']] += 1
+    return counts
+
+def display_log_counts(counts: dict):
+    '''Display log counts in a table.'''
+    print('   Level of log   | Amount')
+    print('------------------|------')
+    for level, count in counts.items():
+        print(f'{level:<17} | {count:<9}')
+
+def main():
+    if len(sys.argv) < 2:
+        print('Usage: python main.py <log_file_path> [<logs.level>]')
+        return
+    
+    log_file_path = sys.argv[1]
+    logs = load_logs(log_file_path)
+
+    if len(sys.argv) == 3:
+        log_level = sys.argv[2].upper()
+        logs = filter_logs_by_level(logs, log_level)
+        print(f"Log details for level '{log_level}':")
+        for log in logs:
+            if log:  # Check if log is not None
+                print(f"{log['datetime']} - {log['message']}")
+                print()
+
+    log_counts = count_logs_by_level([log for log in logs if log])  # Filter out None values
+    display_log_counts(log_counts)
+
+if __name__ == '__main__':
+    main()
+
 
 
 
